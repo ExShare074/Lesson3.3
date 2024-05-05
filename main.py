90def88a7cc35335ee4241e67e58570f1ce1c752
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-05-05 23:50:20 +0300

    add all const

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import pygame
+import random
 
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load("images/tom-clancy-s-rainbow-6-game.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_wigth = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_wigth)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+target_speed = 10
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
 
 running = True
 while running:
