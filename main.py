dbc3d87de3e086a72f6ce417fe0294d5f7ec1c92
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-05-06 12:51:01 +0300

    update code main.py

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 target_speed = 10
 
+score = 0 # счётчик очков
+
+font = pygame.font.Font(None, 36)
 
 running = True
 while running:
@@ -34,6 +37,9 @@ while running:
             if target_x < mouse_x < target_x + target_wigth and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_wigth)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                score += 1
+    text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(text, (10, 10))
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
