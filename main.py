744654ae5249d4e9321ee8de8a4b21ac15294050
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-05-06 12:41:53 +0300

    update code

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("images/tom-clancy-s-rainbow-6-game.jpg")
+icon = pygame.image.load("image/tom.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("images/target.png")
+target_img = pygame.image.load("image/target.png")
 target_wigth = 80
 target_height = 80
 
